dd2649be02355f5edd9a1d02eb848d882fb2dc6b
Nalaaa18 <132159367+Nalaaa18@users.noreply.github.com>
2023-04-30 03:44:48 +0700

    Add files via upload

---
+++ estimasi_goldprice.py
@@ -0,0 +1,21 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_goldprice.sav', 'rb'))
+
+st.title('Estimasi Harga Emas')
+
+Date = st.number_input('Input Tanggal')
+Open = st.number_input('Input Harga Pembukaan Emas')
+High = st.number_input('Input Harga Emas Tertinggi')
+Low = st.number_input('Input Harga Emas Terendah')
+Chg = st.number_input('Input Harga Penutupan Emas')
+
+predict = ''
+
+if st.button('Estimasi Harga'):
+    predict = model.predict(
+        [[Date, Open, High, Low, Chg]]
+    )
+    st.write ('Estimasi harga mobil bekas dalam Ponds : ', predict)
+    st.write ('Estimasi harga mobil bekas dalam IDR (Juta) :', predict*17000)
